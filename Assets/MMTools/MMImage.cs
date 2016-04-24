aee5de6f8c7eb401aef6843c20df6fec3f0d167b
Renaud Forestié <renaud.forestie@gsoi.fr>
2016-04-24 12:02:35 +0200

    Init

---
+++ Assets/MMTools/MMImage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MoreMountains.Tools
+{	
+	/// <summary>
+	/// Various static methods used throughout the Infinite Runner Engine and the Corgi Engine.
+	/// </summary>
+
+	public class MMImage : MonoBehaviour 
+	{
+		/// <summary>
+	    /// Coroutine used to make the character's sprite flicker (when hurt for example).
+	    /// </summary>
+	    public static IEnumerator Flicker(Renderer renderer, Color flickerColor, float flickerSpeed, float flickerDuration)
+	    {
+	        Color initialColor = renderer.material.color;
+	        float flickerStop = Time.time + flickerDuration;
+
+	        while (Time.time<flickerStop)
+	        {
+	            renderer.material.color = initialColor;
+	            yield return new WaitForSeconds(flickerSpeed);
+	            renderer.material.color = flickerColor;
+	            yield return new WaitForSeconds(flickerSpeed);
+	        }
+
+	        renderer.material.color = initialColor;        
+	    }
+	}
+}
+
