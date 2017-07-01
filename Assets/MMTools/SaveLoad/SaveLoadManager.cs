9e2e1837cbc17a688f5a8b549a446f0f65d4b3f0
Renaud Forestié <reuno@reuno.net>
2017-07-01 15:14:41 +0200

    :rocket: Updates SaveLoadManager

--- Assets/MMTools/SaveLoad/SaveLoadManager.cs
+++ Assets/MMTools/SaveLoad/SaveLoadManager.cs
@@ -5,20 +5,23 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace MoreMountains.Tools
 {	
-	public static class SaveManager 
+	/// <summary>
+	/// Allows the save and load of objects in a specific folder and file.
+	/// </summary>
+	public static class SaveLoadManager 
 	{
 		private const string _baseFolderName = "/MMData/";
 		private const string _defaultFolderName = "SaveManager";
 
-		/*protected virtual void Awake()
-		{
-			
-			_saveFileName = _savePath+gameObject.name+".achievements.binary";
-		}*/
-
+		/// <summary>
+		/// Determines the save path to use when loading and saving a file based on a folder name.
+		/// </summary>
+		/// <returns>The save path.</returns>
+		/// <param name="folderName">Folder name.</param>
 		static string DetermineSavePath(string folderName = _defaultFolderName)
 		{
 			string savePath;
+			// depending on the device we're on, we assemble the path
 			if (Application.platform == RuntimePlatform.IPhonePlayer) 
 			{
 				savePath = Application.persistentDataPath + _baseFolderName;
@@ -31,29 +34,44 @@ namespace MoreMountains.Tools
 			return savePath;
 		}
 
+		/// <summary>
+		/// Determines the name of the file to save
+		/// </summary>
+		/// <returns>The save file name.</returns>
+		/// <param name="fileName">File name.</param>
 		static string DetermineSaveFileName(string fileName)
 		{
 			return fileName+".binary";
 		}
 
+		/// <summary>
+		/// Save the specified saveObject, fileName and foldername into a file on disk.
+		/// </summary>
+		/// <param name="saveObject">Save object.</param>
+		/// <param name="fileName">File name.</param>
+		/// <param name="foldername">Foldername.</param>
 		public static void Save(object saveObject, string fileName, string foldername = _defaultFolderName)
 		{
 			string savePath = DetermineSavePath(foldername);
 			string saveFileName = DetermineSaveFileName(fileName);
-
+			// if the directory doesn't already exist, we create it
 			if (!Directory.Exists(savePath))
 			{
-				//MMDebug.DebugLogTime("save : directory doesn't exist, let's create it");
 				Directory.CreateDirectory(savePath);
 			}
+			// we serialize and write our object into a file on disk
 	        BinaryFormatter formatter = new BinaryFormatter();
-			FileStream saveFile = File.Create(saveFileName);
+			FileStream saveFile = File.Create(savePath+saveFileName);
 			formatter.Serialize(saveFile, saveObject);
-
 	        saveFile.Close();
 		}
 
-		public static object Load(object saveObject, string fileName, string foldername = _defaultFolderName)
+		/// <summary>
+		/// Load the specified file based on a file name into a specified folder
+		/// </summary>
+		/// <param name="fileName">File name.</param>
+		/// <param name="foldername">Foldername.</param>
+		public static object Load(string fileName, string foldername = _defaultFolderName)
 		{
 			string savePath = DetermineSavePath(foldername);
 			string saveFileName = savePath + DetermineSaveFileName(fileName);
@@ -63,7 +81,6 @@ namespace MoreMountains.Tools
 			// if the MMSaves directory or the save file doesn't exist, there's nothing to load, we do nothing and exit
 			if (!Directory.Exists(savePath) || !File.Exists(saveFileName))
 			{
-				//MMDebug.DebugLogTime("nothing to load at "+_saveFileName);
 				return null;
 			}
 			BinaryFormatter formatter = new BinaryFormatter();
@@ -72,6 +89,18 @@ namespace MoreMountains.Tools
 	        saveFile.Close();
 
 			return returnObject;
+		}
+
+		/// <summary>
+		/// Removes a save from disk
+		/// </summary>
+		/// <param name="fileName">File name.</param>
+		/// <param name="folderName">Folder name.</param>
+		public static void DeleteSave(string fileName, string folderName = _defaultFolderName)
+		{
+			string savePath = DetermineSavePath(folderName);
+			string saveFileName = DetermineSaveFileName(fileName);
+			File.Delete(savePath+saveFileName);
 		}
 	}
 }
