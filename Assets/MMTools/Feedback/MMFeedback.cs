8335104fc21b1dbe18640967efc4f600b6a1fc87
Renaud Forestié <reuno@reuno.net>
2018-08-10 00:32:02 -0400

    :rocket: Adds MMConeOfVision

---
+++ Assets/MMTools/Feedback/MMFeedback.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using MoreMountains.NiceVibrations;
+
+namespace MoreMountains.Tools
+{
+	[Serializable]
+	public class MMFeedback 
+	{
+		public ParticleSystem Particles;
+		public AudioClip Sfx;
+		public MMCameraShakeProperties CameraShakeProperties;
+		public HapticTypes HapticFeedback = HapticTypes.LightImpact;
+		public bool TriggerFlash;
+		public Color FlashColor = Color.white;
+
+		public virtual void Stop()
+		{
+			//TODO
+			if (Particles != null)
+			{
+				Particles.Stop();
+			}
+		}
+
+		public virtual void Play()
+		{
+			//TODO
+			if (Particles != null)
+			{
+				//Particles.Clear();
+				Particles.Play();	
+			}
+
+			if (CameraShakeProperties.Duration > 0)
+			{
+				MMEventManager.TriggerEvent (new MMCameraShakeEvent (CameraShakeProperties.Duration, CameraShakeProperties.Amplitude, CameraShakeProperties.Frequency));
+			}
+		}
+	}
+}
---
+++ Assets/MMTools/Vision/Editor/MMConeOfVisionInspector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+using MoreMountains.InventoryEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace MoreMountains.Tools
+{
+    [CustomEditor(typeof(MMConeOfVision))]
+    public class MMConeOfVisionInspector : Editor
+    {
+        protected virtual void OnSceneGUI()
+        {
+            // draws a circle around the character to represent the cone of vision's radius
+            MMConeOfVision coneOfVision = (MMConeOfVision)target;
+
+            Handles.color = Color.yellow;
+            Handles.DrawWireArc(coneOfVision.transform.position, Vector3.up, Vector3.forward, 360f, coneOfVision.VisionRadius);
+
+            // draws two lines to mark the vision angle
+            Vector3 visionAngleLeft = MMMaths.DirectionFromAngle(-coneOfVision.VisionAngle / 2f, coneOfVision.EulerAngles.y);
+            Vector3 visionAngleRight = MMMaths.DirectionFromAngle(coneOfVision.VisionAngle / 2f, coneOfVision.EulerAngles.y);
+
+            Handles.DrawLine(coneOfVision.transform.position, coneOfVision.transform.position + visionAngleLeft * coneOfVision.VisionRadius);
+            Handles.DrawLine(coneOfVision.transform.position, coneOfVision.transform.position + visionAngleRight * coneOfVision.VisionRadius);
+
+            foreach (Transform visibleTarget in coneOfVision.VisibleTargets)
+            {
+                Handles.color = Colors.Orange;
+                Handles.DrawLine(coneOfVision.transform.position, visibleTarget.position);
+            }
+        }
+    }
+}
