1e3cfa7b8115e27b46f2f11f2f85239282a05237
Renaud Forestie <renaud.forestie@gameloft.com>
2018-08-22 17:48:28 -0400

    :sparkles: Adds MMTools for 3rd Party : AutoFocus, Chromatic Aberration, Cinemachine Cam Shaker, Lens Distortion

---
+++ Assets/MMToolsForThirdParty/MMAutoFocus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using MoreMountains.Tools;
+
+namespace MoreMountains.ToolsForThirdParty
+{
+    public class MMAutoFocus : MonoBehaviour
+    {
+        // Array of targets
+        public Transform[] focusTargets;
+
+        // Current target
+        public float focusTargetID;
+
+        // Cache profile
+        PostProcessVolume _volume;
+        PostProcessProfile _profile;
+        DepthOfField _depthOfField;
+
+        // Adjustable aperture - used in animations within Timeline
+        [Range(0.1f, 20f)] public float aperture;
+
+
+        void Start()
+        {
+            _volume = GetComponent<PostProcessVolume>();
+            _profile = _volume.profile;
+
+            _profile.TryGetSettings<DepthOfField>(out _depthOfField);
+
+
+        }
+
+        void Update()
+        {
+            // Get distance from camera and target
+            float dist = Vector3.Distance(transform.position, focusTargets[Mathf.FloorToInt(focusTargetID)].position);
+
+            // Set variables
+            _depthOfField.focusDistance.Override(dist);
+            _depthOfField.aperture.Override(aperture);
+        }
+    }
+}
---
+++ Assets/MMToolsForThirdParty/MMLensDistortion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using MoreMountains.Tools;
+
+namespace MoreMountains.ToolsForThirdParty
+{
+    public class MMLensDistortion : MonoBehaviour
+    {
+        protected LensDistortion _lensDistortion;
+        protected PostProcessVolume _volume;
+
+        protected virtual void Awake()
+        {
+            _volume = this.gameObject.GetComponent<PostProcessVolume>();
+            _volume.profile.TryGetSettings(out _lensDistortion);
+        }
+
+        public virtual void SetIntensity(float newIntensity)
+        {
+            _lensDistortion.intensity.Override(newIntensity);
+        }
+	}
+}
